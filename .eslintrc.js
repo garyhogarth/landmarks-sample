335db22a791410d87faa98e349dd81c99111f82a
Gary Hogarth <gary.hogarth@tomtom.com>
2022-03-05 16:51:13 +0000

    Add import order lint rules and update author in package
    For quick PoCs I tend to use https://github.com/ReactNativeSchool/react-native-school-expo-template. It just saves time setting up lint/react nav etc.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,36 @@ module.exports = {
     'react/style-prop-object': 0,
     'react/require-default-props': 0,
     'import/extensions': 0,
+    // import plugins
+    'import/no-unresolved': 'error',
+    'import/named': 'error',
+    'import/namespace': 'error',
+    'import/default': 'error',
+    'import/export': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
   },
+  plugins: [
+    'import', // eslint-plugin-import for custom configure
+  ],
   globals: {
     __DEV__: 'readonly',
   },
-  plugins: [],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
 };
